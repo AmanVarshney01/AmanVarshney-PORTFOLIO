7a97eb739959496ad927dee1447325fa03cd4d4e
Aman Varshney <drawscale@gmail.com>
2019-08-14 11:07:18 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7ac2b7cc5b51b0a9a05646b163a02fce812e17419c48ca24'
+SECRET_KEY = 'SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
